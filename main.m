8c35e41197a81d0ed5ed8fdfef50ded2f6770871
Nathan Dauber <Nathan.Dauber@gmail.com>
2012-05-06 21:52:00 -0500

    fixed a bug with calculating the atmospheric light

--- main.m
+++ main.m
@@ -1,6 +1,10 @@
-image = im2double(imread('canon7/canon7.jpg'));
-%image = im2double(imread('Untitled.png'));
-dc = dark_channel(image);
-al = estimate_atmospheric_light(dc, image);
-t = transmission(image, al);
-r = scene_radiance(image, al, t);
+function [] = main(loc)
+    image = im2double(imread(loc));
+    %image = im2double(imread('Untitled.png'));
+    dc = dark_channel(image);
+    al = estimate_atmospheric_light(dc, image);
+    t = transmission(image, al);
+    r = scene_radiance(image, al, t);
+    subplot(1, 2, 1), imshow(image);
+    subplot(1, 2, 2), imshow(r);
+end
